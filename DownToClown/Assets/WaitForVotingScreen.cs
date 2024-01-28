d640a544a8cc5b39b443394cec8d2b8e6e914d95
chkohtz <84747967+chkohtz@users.noreply.github.com>
2024-01-27 19:53:42 -0500

    removed timer for voting

--- DownToClown/Assets/WaitForVotingScreen.cs
+++ DownToClown/Assets/WaitForVotingScreen.cs
@@ -6,50 +6,10 @@ using UnityEngine.Events;
 
 public class WaitForVotingScreen : GameScreen
 {
-
-    public bool onCountdown;
-    public TextMeshProUGUI countdownTimer;
-    public float timerCooldown = 60f;
-    public float timer = 60f;
-    public UnityEvent onTimerEnd;
+    public UnityEvent onVotingEnd;
 
     public override void Update()
     {
-        UpdateCountdown();
-    }
 
-    public void UpdateCountdown()
-    {
-        if (onCountdown)
-        {
-            timer -= Time.deltaTime;
-            countdownTimer.text = Mathf.CeilToInt(timer).ToString();
-
-            if (Mathf.CeilToInt(timer) <= 0)
-            {
-                onTimerEnd.Invoke();
-                Advance();
-            }
-        }
-    }
-
-    public void BeginCountdown()
-    {
-        onCountdown = true;
-        countdownTimer.gameObject.SetActive(true);
-    }
-
-    public void ResetCountdown()
-    {
-        timer = timerCooldown;
-        onCountdown = false;
-        countdownTimer.gameObject.SetActive(false);
-
-    }
-
-
-    public void Advance()
-    {
-        //whatever you need to do once time runs out or everyone responded
     }
 }
