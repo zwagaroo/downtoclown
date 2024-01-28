035d4faaadf59f19e6148db348e9468b3958146b
chkohtz <84747967+chkohtz@users.noreply.github.com>
2024-01-27 21:12:10 -0500

    emojis
    added a package and stuff yippeee

--- DownToClown/Assets/RoundResultsScreen.cs
+++ DownToClown/Assets/RoundResultsScreen.cs
@@ -1,13 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RoundResultsScreen : GameScreen
 {
     public GameObject playerPrefab;
+    public bool onCountdown;
+    public TextMeshProUGUI countdownTimer;
+    public float timerCooldown = 5f;
+    public float timer = 5f;
+    public UnityEvent OnTimerEnd;
+    public override void Update()
+    {
+        UpdateCountdown();
+    }
 
     public void DisplayWinner()
     {
-        //display winner
+        
     }
+    public void UpdateCountdown()
+    {
+        if (onCountdown)
+        {
+            timer -= Time.deltaTime;
+
+            if (Mathf.CeilToInt(timer) <= 0)
+            {
+                OnTimerEnd.Invoke();
+            }
+        }
+    }
+
+    public void BeginCountdown()
+    {
+        onCountdown = true;
+    }
+
+    public void ResetCountdown()
+    {
+        timer = timerCooldown;
+        onCountdown = false;
+
+    }
+
 }
