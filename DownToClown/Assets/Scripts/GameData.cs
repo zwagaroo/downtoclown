f103af4f8a437767a8ba790c8120d54a4608a3b4
Teo Miklethun <teomi@umich.edu>
2024-01-28 13:36:26 -0500

    character constructer with sound replacement.

--- DownToClown/Assets/Scripts/GameData.cs
+++ DownToClown/Assets/Scripts/GameData.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using Random = System.Random;
 
 [Serializable]
 public class GameData
@@ -42,5 +45,71 @@ public class Character
     public Sprite GetProfileSprite()
     {
         return LoadSprite(profileImage);
+    }
+
+    public Character(Character character)
+    {
+        this.id = character.id;
+        this.name = character.name;
+        this.description = character.description;
+        this.rule = ReplaceSounds(character.rule);
+
+        this.iconImage = character.iconImage;
+        this.descriptionImage = character.descriptionImage;
+        this.profileImage = character.profileImage;
+    }
+
+    public static string[] clownSoundEffects = {
+        "Honk",
+        "Squeak",
+        "Boing",
+        "Crash",
+        "Bang",
+        "Zap",
+        "Pop",
+        "Bzzt",
+        "Ding",
+        "Toot",
+        "Clang",
+        "Whir",
+        "Thunk",
+        "Twang",
+        "Splash",
+        "Whoop",
+        "Hoot",
+        "Chirp",
+        "Clink",
+        "Splat",
+        "Thud",
+        "Wham",
+        "Zoom",
+        "Bam",
+        "Jingle",
+        "Giggle",
+        "Hiccup",
+        "Chuckle",
+        "Sizzle",
+        "Mwah"
+    };
+
+    static string ReplaceSounds(string text)
+    {
+        int seed = (int)DateTime.Now.Ticks & 0x0000FFFF;
+        Random random = new Random(seed);
+        string pattern = @"\{sound\}";
+
+        // Shuffle the array of replacements
+        string[] shuffledReplacements = clownSoundEffects.OrderBy(x => random.Next()).ToArray();
+
+        int currentIndex = 0;
+        return Regex.Replace(text, pattern, m => {
+            // If we reached the end of the replacements, reset the index and reshuffle the array
+            if (currentIndex >= shuffledReplacements.Length)
+            {
+                currentIndex = 0;
+                shuffledReplacements = clownSoundEffects.OrderBy(x => random.Next()).ToArray();
+            }
+            return shuffledReplacements[currentIndex++];
+        });
     }
 }
